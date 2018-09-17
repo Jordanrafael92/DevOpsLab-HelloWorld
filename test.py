b19548a9ef0f015084018ebc695f31a659fe0f9d
Jordan Silva <jordanrafael92@gmail.com>
2018-09-17 14:28:50 -0300

    Alteração do Título
    Alteração do Título da Página

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials")
+        self.assertRegex(result.data.decode(), "DevOps Essentials - Teste Jordan 17/09/2018")
 
 
 if __name__ == "__main__":
